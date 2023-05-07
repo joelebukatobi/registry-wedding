7b142156b42c04e39e597136179d5efd5f9443cd
Joel Onwuanaku <joelebuka@gmail.com>
2023-05-07 15:01:34 +0100

    Updated the RSVP

--- src/components/layouts/rsvp.js
+++ src/components/layouts/rsvp.js
@@ -15,8 +15,8 @@ const RSVP = ({ AOS }) => {
           {`   We hope you can join us on our special day! To let us know if you can make it, please RSVP by [date] by
           clicking the button below. We kindly ask that you include the names of all guests attending in your response.
           If you have any questions or need to make changes to your RSVP, please don't hesitate to contact us. You can
-          reach us by phone at [phone number] or by email at [email address]. In case of an emergency or last-minute
-          changes, please contact our wedding coordinator at [phone number]. We can't wait to celebrate with you! sem.`}
+          reach us by phone on +2349038395532 or 
+          +2348170437946`}
         </p>
       </header>
     </section>
